fcd44252edebf3924b21e3a11b6e3f9dd3258b01
Talon Durrant <talonmdurrant@gmail.com>
2022-11-10 23:21:20 -0700

    create seasonals page in cms

---
+++ content/pages/seasonals.md
@@ -0,0 +1,10 @@
+---
+name: Seasonals
+content:
+  heading: Under Construction
+  content: This page is currently under construction. Thank you for your patience
+    as we get it ready.
+metadata:
+  title: Seasonals
+  slug: seasonals
+---
--- pages/seasonals.tsx
+++ pages/seasonals.tsx
@@ -1,23 +1,51 @@
 import { NextPage } from 'next';
 
 import PageLayout from '../layouts/PageLayout';
+import { Metadata } from '../layouts/components/Seo';
 
-const SeasonalsPage: NextPage = () => {
+import MarkdownText from '../components/markdown-text';
+
+interface SeasonalsPageProps {
+  name: string;
+  metadata: Metadata;
+  content: {
+    heading: string;
+    content: string;
+  };
+}
+
+const SeasonalsPage: NextPage<SeasonalsPageProps> = ({ name, metadata, content }) => {
   return (
-    <PageLayout seo={{ title: 'Seasonals', slug: 'seasonals' }}>
+    <PageLayout seo={metadata}>
       <div className="relative h-64 overflow-clip">
         <div className="absolute inset-0">
           <img className="object-cover w-full h-full" src="/assets/hero-image.jpg" alt="red and white flowers closeup" />
         </div>
       </div>
-      <h1 className="mx-auto my-8 text-4xl font-bold text-primary max-w-7xl">Under Construction</h1>
-      <section className="mx-auto my-8 max-w-7xl">
+      <h1 className="px-4 mx-auto my-8 text-4xl font-bold text-primary max-w-7xl">{content.heading}</h1>
+      <section className="px-4 mx-auto my-8 max-w-7xl">
         <div className="space-y-4">
-          <p>This page is currently under construction. Thank you for your patience as we get it ready.</p>
+          <MarkdownText>{content.content}</MarkdownText>
         </div>
       </section>
     </PageLayout>
   );
 };
+
+export async function getStaticProps() {
+  const page = await import('../content/pages/seasonals.md').catch(error => null);
+
+  if (!page) return { props: {} };
+
+  const { name, metadata, content } = page.attributes;
+
+  return {
+    props: {
+      name,
+      metadata,
+      content,
+    },
+  };
+}
 
 export default SeasonalsPage;
