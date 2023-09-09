f54a9c1ae40c6e2306e77a03d94e189af0e3b8c8
Talon Durrant <talonmdurrant@gmail.com>
2023-09-09 17:58:50 -0600

    add testimonals to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,6 +39,15 @@ interface IndexPageProps {
       href: string;
     };
   };
+  testimonials: {
+    heading: string;
+    reviews: {
+      stars: number;
+      when: string;
+      content: string;
+      author: string;
+    }[];
+  };
   gallery: {
     images: {
       source: string;
@@ -49,7 +58,7 @@ interface IndexPageProps {
   };
 }
 
-const IndexPage: NextPage<IndexPageProps> = ({ name, metadata, content, businessDetails, pricelist, gallery }) => {
+const IndexPage: NextPage<IndexPageProps> = ({ name, metadata, content, businessDetails, pricelist, testimonials, gallery }) => {
   return (
     <PageLayout seo={metadata}>
       <div className="relative">
@@ -109,9 +118,55 @@ const IndexPage: NextPage<IndexPageProps> = ({ name, metadata, content, business
         </div>
       </section>
 
+      <section className="px-4 py-16 my-8 bg-primary text-light">
+        <h2 className="pb-8 mx-auto text-3xl font-bold max-w-7xl">{testimonials.heading}</h2>
+        <div className="grid gap-16 mx-auto max-w-7xl md:grid-cols-3">
+          {testimonials.reviews.map((review, index) => (
+            <div key={`${review.author} ${index}`} className="">
+              <div className="relative px-8 pt-3">
+                <span className="absolute top-0 left-0 text-7xl">&ldquo;</span>
+                <NewlineText>{review.content}</NewlineText>
+              </div>
+              <p className="my-4 italic">
+                <span className="font-bold">By {review.author}</span> - {review.when}
+              </p>
+              <div className="flex my-4">
+                {Array.from({ length: review.stars }, (_, i) => (
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                    <path
+                      fillRule="evenodd"
+                      d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                )).map(star => star)}
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="mx-auto mt-16 space-x-8 max-w-7xl">
+          <a
+            className="px-4 py-2 border-2 rounded-full bg-light text-primary hover:bg-primary hover:text-light hover:border-light"
+            href="https://www.google.com/search?hl=en-US&gl=us&q=Sandia+Nursery,+Washington,+UT+84780&ludocid=17350137008794683629&lsig=AB86z5XjOSqfebZg0dvV-XvI4kK7#lrd=0x80ca50a0fa23009f:0xf0c811af3d90c8ed,3"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Write a Review
+          </a>
+          <a
+            className="px-4 py-2 border-2 rounded-full bg-primary text-light border-light hover:bg-light hover:text-primary"
+            href="https://www.google.com/search?hl=en-US&gl=us&q=Sandia+Nursery,+Washington,+UT+84780&ludocid=17350137008794683629&lsig=AB86z5XjOSqfebZg0dvV-XvI4kK7#lrd=0x80ca50a0fa23009f:0xf0c811af3d90c8ed,1"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Read More
+          </a>
+        </div>
+      </section>
+
       <section className="px-4 mx-auto my-16 max-w-7xl">
         <h2 className="mb-8 text-3xl font-bold text-primary">Gallery</h2>
-        <Gallery data={{ type: 'gallery', images: gallery.images }} />
+        <Gallery images={gallery.images} />
       </section>
     </PageLayout>
   );
@@ -122,7 +177,7 @@ export const getStaticProps = async () => {
 
   if (!page) return { props: {} };
 
-  const { name, metadata, content, businessDetails, pricelist, gallery } = page.attributes;
+  const { name, metadata, content, businessDetails, pricelist, testimonials, gallery } = page.attributes;
 
   return {
     props: {
@@ -131,6 +186,7 @@ export const getStaticProps = async () => {
       content,
       businessDetails,
       pricelist,
+      testimonials,
       gallery,
     },
   };
